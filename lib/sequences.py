03342b98e16d3adf4d5c90c5cb40cb4c09d7fed2
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-05-09 20:52:05 +0300

    Implement Fibonacci sequence generator and add binary test files

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib = []
+    if length > 0:
+        fib.append(0)
+    if length > 1:
+        fib.append(1)
+    for i in range(2, length):
+        fib.append(fib[i-1] + fib[i-2])
+    print(f"{fib}")
+
     pass
